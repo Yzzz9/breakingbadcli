ac60f10e6296741ef17a00d48e7105aba299a803
Yzzz9 <61928128+Yzzz9@users.noreply.github.com>
2021-09-28 11:40:50 +0530

    added data.go

---
+++ data/data.go
@@ -0,0 +1,13 @@
+package data
+
+var Episode int
+
+type Response struct {
+    Eid int `json:"episode_id"`
+    Title string `json:"title"`
+    Season string `json:"season"`
+    Epnum string `json:"episode"`
+    Series string `json:"series"`
+    Airdate string `json:"air_date"`
+    Characters []string `json:"characters"`
+}
